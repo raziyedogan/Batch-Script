19d8033cca36a1a958becea5d9e63865e11d4a6c
RAZİYE DOGAN <98969893+raziyedogan@users.noreply.github.com>
2023-07-27 10:32:59 +0300

    Add files via upload

---
+++ cd_command/cd.bat
@@ -0,0 +1,16 @@
+@echo off
+Rem Used to display the current working directory.
+cd
+Rem Changing the path to Program Files
+cd\Program Files
+cd
+Rem Changing the path to Program Files
+cd %USERPROFILE%
+cd
+Rem Changing to the parent directory
+cd..
+cd
+Rem Changing to the parent directory two levels up
+cd..\..
+cd
+pause
