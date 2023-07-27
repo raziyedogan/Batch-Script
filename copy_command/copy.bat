a0871302fe9cbe3077f13958315475db80895caf
RAZİYE DOGAN <98969893+raziyedogan@users.noreply.github.com>
2023-07-27 10:41:16 +0300

    Add files via upload

---
+++ copy_command/copy.bat
@@ -0,0 +1,15 @@
+@echo off
+cd
+Rem Copies lists.txt to the present working directory.
+Rem If there is no destination identified , it defaults to the present working directory.
+copy c:\example.txt
+Rem The file lists.txt will be copied from C:\ to C:\tp location
+copy C:\example.txt c:\tp
+Rem Quotation marks are required if the file name contains spaces
+copy “C:\example file.txt”
+Rem Copies all the files in F drive which have the txt file extension to the
+current working directory copy
+F:\*.txt
+Rem Copies all files from dirA to dirB. Note that directories nested in dirA will not be copied
+copy C:\dirA dirB
+pause
